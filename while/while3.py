f2fba28e9fe5c1fd27e114244aefb97cb4555306
Alexander Adrian <mongtarget@gmail.com>
2025-09-27 22:21:20 -0500

    Segunda subida

---
+++ while/while3.py
@@ -0,0 +1,11 @@
+x = 1
+suma = 0
+
+while x<= 10:
+    valor = int(input("Ingrese el valor:"))
+    x = x + 1
+
+promedio = suma / 10
+print("La suma de los valores es de:",suma)
+print("El promedio de los valores es de:",promedio)
+
