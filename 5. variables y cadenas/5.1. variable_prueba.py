a46494acca9fe8540eae713cee031417bb00c2dd
Mongtarget <mongtarget@gmail.com>
2025-10-14 18:06:47 -0500

    lol

---
+++ 5. variables y cadenas/5.1. variable_prueba.py
@@ -0,0 +1,13 @@
+#int
+cantidad = int(input("Ingrese una cantidad: "))
+#float
+precio = float(input("Ingrese un precio: "))
+#str
+st("SUS")
+#input
+nombre = input("Ingrese su nombre: ")
+#bool
+es_valido = True
+no_es_valido = False
+#list
+colores = ["rojo", "verde", "azul"]
